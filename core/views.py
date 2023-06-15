32509c27abf1dda1490d0e530b686a9e5b1ecade
Augusto Nonato Moraes <devaugusto.moraes@gmail.com>
2023-06-14 21:36:11 -0300

    salvando produtos no banco de dados e criando pasta media no projeto pra salvar as imagens cadastradas

--- core/views.py
+++ core/views.py
@@ -36,8 +36,8 @@ def contato(requests):
 def produto(request):
     if str(request.method) == 'POST':
         form = ProdutoModelForm(request.POST, request.FILES)
-        print(f"Validação do form: {form.is_valid()}")
         if form.is_valid():
+            """
             prod = form.save(commit=False)
 
             print(f'Nome: {prod.nome}')
@@ -45,6 +45,8 @@ def produto(request):
             print(f'Estoque: {prod.estoque}')
             print(f'Imagem: {prod.imagem}')
             
+            """      
+            form.save()
             messages.success(request, 'Produto salvo com sucesso.')
             form = ProdutoModelForm()
         else:
